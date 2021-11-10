06e849e4260094910f3d0a62e36ef248d65643df
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-11-10 08:16:50 -0300

    feat: Install and configure storybook ✨

---
+++ .storybook/preview.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { RouterContext } from "next/dist/shared/lib/router-context";
+import GlobalStyles from "../src/styles/global";
+import { ThemeProvider } from "styled-components";
+import theme from "../src/styles/theme";
+
+export const parameters = {
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Story />
+      </ThemeProvider>
+    </>
+  ),
+];
