116df5955c77bed9eb5e80dc15c3a01fa6c9d930
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-11-09 21:49:59 -0300

    feat: Create theme, Globalstyles and configure types from styled-components ✨

---
+++ src/styles/global.ts
@@ -0,0 +1,56 @@
+import { createGlobalStyle, css } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 300;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/Montserrat-Light.ttf') format('ttf'),
+  }
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/Montserrat-Regular.ttf') format('ttf'),
+  }
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 600;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/Montserrat-Bold.ttf') format('ttf'),
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+        
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
+  }
+
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%;
+    }
+
+    body {
+      font-family: ${theme.font.family};
+    }
+  `}
+`;
+
+export default GlobalStyles;
---
+++ src/styles/theme.ts
@@ -0,0 +1,23 @@
+export default {
+  font: {
+    family: "Montserrat, sans-serif",
+    light: 300,
+    normal: 400,
+    bold: 600,
+    sizes: {
+      xsmall: "1.2rem",
+      small: "1.4rem",
+      medium: "1.6rem",
+      large: "2.0rem",
+      xlarge: "2.8rem",
+    },
+  },
+  colors: {
+    purple: "#A141EB",
+    purple_dark: "#7928B7",
+    yellow: "#E1A607",
+    blue: "#0CBFE2",
+    background: "#F1F1F1",
+    dark: "#5B5B5B",
+  },
+} as const;
